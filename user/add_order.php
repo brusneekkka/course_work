d4a52571dc7a6dfd2c38124495478b8ad87b13b9
brusneekkka <118313689+brusneekkka@users.noreply.github.com>
2023-01-17 12:10:07 +0300

    Update add_order.php

--- user/add_order.php
+++ user/add_order.php
@@ -3,16 +3,7 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'\course_work\json_encode.php');
     
     $name = $_GET['name'];
-    //$number_order = $_GET['number_order'];
-    //$numbers_products = $_GET['numbers_products'];
-    //проверка на не ноль и на то что они есть
     $db = dbconnection();
-    /*foreach ($numbers_products as $number_product) {
-        echo $number_product;
-        $sql = "INSERT INTO orders (name, number_order, number_product) VALUES ('$name','$number_order','$number_product')";
-        $res = mysqli_query($db, $sql);
-    }
-    */
     $sql = "INSERT INTO orderss (name) VALUES ('$name')";
         $res = mysqli_query($db, $sql);
     if($res)
