96bc0de994f3c0e3edb6ba27345156a1c7cc6ce4
brusneekkka <118313689+brusneekkka@users.noreply.github.com>
2023-01-17 12:14:20 +0300

    Update delete_order.php

--- user/delete_order.php
+++ user/delete_order.php
@@ -3,10 +3,7 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'\course_work\json_encode.php');
     
     $number_order = $_GET['number_order'];
-    
-    //проверка на не ноль и на то что они есть
     $db = dbconnection();
-
     $sql = "DELETE FROM orders WHERE number_order = '$number_order'";
     $res = mysqli_query($db, $sql);
     if($res)
